13d4e4911005ff2cd770eb47f17ad67cf73577d1
Petar Kresoja <petarkresoja5@gmail.com>
2023-01-24 14:32:36 +0100

    Now using external image source

--- src/services/ImageService.js
+++ src/services/ImageService.js
@@ -1,9 +1,5 @@
 export default {
     getDestinationImageUrl(dest) {
-        try {
-            return require('@/assets/img/destination/' + dest.toLowerCase().split(' ')[0] + '.jpg');
-        } catch (error) {
-            return require('@/assets/img/destination/default.jpg')
-        }
+        return 'https://img.pequla.com/destination/' + dest.toLowerCase().split(' ')[0] + '.jpg';
     }
 }
